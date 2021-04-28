c6b6a88a905a93de5323d185ee4e0fdb3231c0e0
Giovanny Zaruma <geos_20@yahoo.com>
2021-04-27 23:52:38 -0500

    Deber 3 Giovanny Zaruma

---
+++ Deber3GiovannyZ/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Deber3GiovannyZ
+{
+    public partial class MainPage : ContentPage
+    {
+        public static String NombreUsuario = "Giovanny";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string clave = txtPass.Text;
+
+            if ((usuario == "Giovanny") && (clave == "Zaruma"))
+            {
+                await Navigation.PushAsync(new Pagina2(usuario, clave));
+            }
+            else
+            {
+                lblResultado.Text = "El usuario o contraseña es incorrecto";
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Deber3GiovannyZ/Pagina2.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Deber3GiovannyZ
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina2 : ContentPage
+    {
+        public Pagina2(string usuario, string clave)
+        {
+            InitializeComponent();
+            //recibe el datos
+            lblUser.Text = usuario;
+
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTotal_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double s1 = Convert.ToDouble(TxtDato1.Text);
+                double exs1 = Convert.ToDouble(TxtDato2.Text);
+                double s2 = Convert.ToDouble(TxtDato3.Text);
+                double ex2 = Convert.ToDouble(TxtDato4.Text);
+
+                double total = ((s1 * 0.3) + (exs1 * 0.2)) + ((s2 * 0.3) + (ex2 * 0.2));
+
+                TxtTotal.Text = total.ToString();
+                if (total >= 7)
+                {
+                    DisplayAlert("Alerta", "Aprobado", "Gracias");
+                }
+                else
+                {
+                    DisplayAlert("Alerta", "Reprobado", "Gracias");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+    }
+}
